1047e5c998be971b5079c8635edba22664daec8d
ShortJohn <geor.kontogiannis@gmail.com>
2025-05-31 17:46:28 +0300

    Added configure file to automatically update versioning

--- app/repl/main.cpp
+++ app/repl/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "repl.hpp"
+#include "config.hpp"
 
 int main(void)
 {
-    std::cout << "Welcome to Monkey REPL. Feel free to type anything" << std::endl;
+    std::cout << "Welcome to " << project_name << " " << 
+            project_version_major << "." <<
+            project_version_minor << "." <<
+            project_version_patch << std::endl;
+            std::cout << "Feel free to type anything!" << std::endl;
 
     Repl repl;
     repl.start();
